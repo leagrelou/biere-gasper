c8f074fc342929bc3164a185e71bb423e0d9d416
Lea Grelou <leagrelou@gmail.com>
2020-01-31 00:03:01 -0500

    changed the banner photo and started filling the seeds for the location model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create!(name:'Le Saint-Jean-de-Luz', address:'11 rue Léon Blum, Amiens')
